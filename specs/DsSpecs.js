16f101f7c399b8e2381ad7d43f820f0cf37b7600
MAKEM <makem@ukr.net>
2015-07-02 22:41:49 +0200

    Data Space asking implementation

--- specs/DsSpecs.js
+++ specs/DsSpecs.js
@@ -1,27 +1,47 @@
 /**
  * Created by M.Vyhovskyi on 7/2/2015.
  */
+
 var expect = require('./util/expect');
 var ds = require('../src/ds');
 
+const readQuery = "match a->b->c return a";
+
 describe("Data Space API",function(){
-    describe("when query data",function(){
-        it("validates request first");
+    describe("when querying data",function(){
+        describe("and validates request first",function(){
+            it("then applies string expressions only",function(){
+               ds.ask(readQuery,function(){});
+            });
+            it("then throws if expression is not specified",function(){
+                var ask = expect(function(){
+                    ds.ask({something:"other than expression string"},function(){});
+                });
+                ask().to.throw(Error);
+            })
+        });
         it("checks callback is specified",function(){
-            var asking = function(){ds.ask("match a->b->c return a")};
-            var ask = expect(asking);
+            var ask = expect(function(){
+                ds.ask(readQuery)
+            });
             ask().to.throw(Error);
         });
         it("checks callback is not null",function(){
-            var asking = function(){ds.ask("match a->b->c return a",null)};
-            var ask = expect(asking);
+            var ask = expect(function(){
+                ds.ask(readQuery,null)
+            });
             ask().to.throw(Error);
         });
         it("checks callback is a function",function(){
-            var asking = function(){ds.ask("match a->b->c return a",123)};
-            var ask = expect(asking);
+            var ask = expect(function(){
+                ds.ask(readQuery,"something except a callback");
+            });
             ask().to.throw(Error);
         });
-        it("performs request and callback after it's complete");
+        it("performs request and callback after it's complete",function(done){
+            ds.ask(readQuery,function(){
+                done();
+            });
+        });
     });
 });
--- src/ds.js
+++ src/ds.js
@@ -2,14 +2,26 @@
  * Created by M.Vyhovskyi on 7/2/2015.
  */
 
-function ask(query, cb){
+function verifyCallbackIsSpecified(cb){
     if(cb == 'undefined' ||
-       cb == null ||
-       !(cb instanceof Function)){
+        cb == null ||
+        !(cb instanceof Function)){
         throw Error('A query callback is undefined');
     }
 }
 
+function verifyExpression (expression) {
+    if(typeof expression !== 'string'){
+        throw Error("An expression should be a valid query string");
+    }
+}
+
+function ask(expression, cb){
+    verifyCallbackIsSpecified(cb);
+    verifyExpression(expression);
+    cb(null);
+}
+
 module.exports = {
     ask: ask
-}
+};
